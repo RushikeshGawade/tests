2cafd59e5dd38ab4a58168c90fba3d333c74b695
Rushikesh G <rushikeshg@tataclassedge.com>
2021-06-19 12:15:51 +0530

    Final Output

--- src/main/java/com/truemeds/test/api/TrueMedsController.java
+++ src/main/java/com/truemeds/test/api/TrueMedsController.java
@@ -1,9 +1,5 @@
 package com.truemeds.test.api;
 
-import com.truemeds.test.model.InputDetails;
-import com.truemeds.test.repository.InputDetailsRepository;
-import com.truemeds.test.service.ProcessingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,19 +8,28 @@ import java.util.List;
 @RestController
 public class TrueMedsController {
 
-    @Autowired
-    private ProcessingService processingService;
-    private InputDetailsRepository inputDetailsRepository;
-
-    public TrueMedsController(InputDetailsRepository inputDetailsRepository) {
-        this.inputDetailsRepository = inputDetailsRepository;
-    }
-
-    @GetMapping("/getDetails")
-    public List<InputDetails> getDetails() {
-        List<InputDetails> details = inputDetailsRepository.findAll();
-        System.out.println(details);
-        processingService.processData();
-        return details;
+    @GetMapping("/showLogic")
+    public String getDetails() {
+        return " static String removeDuplicateHelper(String str, char lastRemoved) {\n" +
+                "        if (str.length() == 0 || str.length() == 1)\n" +
+                "            return str;\n" +
+                "        if (str.charAt(0) == str.charAt(1)) {\n" +
+                "            lastRemoved = str.charAt(0);\n" +
+                "            while (str.length() > 1 && str.charAt(0) == str.charAt(1))\n" +
+                "                str = str.substring(1, str.length());\n" +
+                "            str = str.substring(1, str.length());\n" +
+                "            return removeDuplicateHelper(str, lastRemoved);\n" +
+                "        }\n" +
+                "        String rem_str = removeDuplicateHelper(str.\n" +
+                "                substring(1, str.length()), lastRemoved);\n" +
+                "        if (rem_str.length() != 0 && rem_str.charAt(0) == str.charAt(0)) {\n" +
+                "            lastRemoved = str.charAt(0);\n" +
+                "\n" +
+                "            return rem_str.substring(1, rem_str.length());\n" +
+                "        }\n" +
+                "        if (rem_str.length() == 0 && lastRemoved == str.charAt(0))\n" +
+                "            return rem_str;\n" +
+                "        return (str.charAt(0) + rem_str);\n" +
+                "    }";
     }
 }
